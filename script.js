a50980d63dfc8ff9e64347163d7774c8e6e03d51
mcarter78 <sjsharksftw@gmail.com>
2016-01-25 11:19:20 -0800

    adds files and content

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript to be added
+window.onload = function() {
+  var mailingListButton = document.querySelector('#button-yellow');
+
+  mailingListButton.addEventListener("click", function(e){
+    window.prompt("Please Enter Your Email");
+  });
+}
