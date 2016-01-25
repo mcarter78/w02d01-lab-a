e5e53c76d53d62b7e145ab42269d33a168780b36
danielszeto <d.szeto123@gmail.com>
2016-01-25 11:43:52 -0800

    adds thanks for your email

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 // JavaScript to be added
+
+// var mailingListButton = document.querySelector('#button-yellow');
+// var email;
+
+
 window.onload = function() {
-  var mailingListButton = document.querySelector('#button-yellow');
+  var mailingListButton = document.getElementById('button-yellow');
 
   mailingListButton.addEventListener("click", function(e){
-    window.prompt("Please Enter Your Email");
+    var email = window.prompt("Please Enter Your Email");
+    console.log(mailingListButton);
+
+    mailingListButton.innerHTML = "Thanks for your email";
+    	
+   
   });
-}
+    
+
+};
